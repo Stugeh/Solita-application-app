c2befe295e3a3e15aa8bb84825d25f1681189aa4
stuge <tuukkaveteli@gmail.com>
2021-01-19 19:15:51 +0200

    front end fetches list of names from json server.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  // const [person, setPerson] = useState('');
+  const [people, setPeople] = useState('');
+
+  useEffect(async () => {
+    const persons = await axios.get('http://localhost:3001/names');
+    setPeople(persons.data);
+  }, []);
+
+  // eslint-disable-next-line
+  console.log('people :>> ', people);
+
   return (
     <div>
       hello world
     </div>
-  )
-}
+  );
+};
 
 export default App;
