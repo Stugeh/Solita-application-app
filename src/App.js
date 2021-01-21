fb7f7277076291821518f1821e677c98ae2b0b8a
stuge <tuukkaveteli@gmail.com>
2021-01-21 14:06:20 +0200

    renders unsorted table. added css to make it look nice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+/* eslint react/prop-types: 0 */
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// Run the json server : npx json-server --port=3001 --watch names.json
+
+const RenderPerson = ({ person }) => (
+  <tr>
+    <td>{person.name}</td>
+    <td>{person.amount}</td>
+  </tr>
+);
+
 const App = () => {
-  // const [person, setPerson] = useState('');
-  const [people, setPeople] = useState('');
+  const [people, setPeople] = useState([]);
 
   useEffect(async () => {
     const persons = await axios.get('http://localhost:3001/names');
     setPeople(persons.data);
   }, []);
 
-  // eslint-disable-next-line
-  console.log('people :>> ', people);
-
   return (
-    <div>
-      hello world
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>NAME</th>
+          <th>NUMBER</th>
+        </tr>
+      </thead>
+      <tbody>
+        { people.map((person) => <RenderPerson key={person.name} person={person} />)}
+      </tbody>
+    </table>
   );
 };
 
