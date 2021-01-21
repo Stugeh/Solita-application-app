652a1c7cd805b991d55b2f3656dac2b8f26a8e5b
stuge <tuukkaveteli@gmail.com>
2021-01-21 16:27:09 +0200

    css styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// Run the json server : npx json-server --port=3001 --watch names.json
+// Run the json-server: npx json-server --port=3001 --watch names.json
 
 const RenderPerson = ({ person }) => (
   <tr>
@@ -13,14 +13,18 @@ const RenderPerson = ({ person }) => (
 );
 
 const App = () => {
+  // The list of people fetched from the json server gets
+  // stored in "people" as an array of objects.
+  // "search" tracks searchbox state.
   const [people, setPeople] = useState([]);
   const [search, setSearch] = useState('');
-
+  // array of people that contains the results of the search
   const filteredPeople = search === '' ? people
     : people.filter(
       (person) => person.name.toLowerCase().includes(search.toLowerCase()),
     );
 
+  // Effect hook to get list of names from json server.
   useEffect(async () => {
     const persons = await axios.get('http://localhost:3001/names');
     setPeople(Object.values(persons.data));
@@ -30,7 +34,7 @@ const App = () => {
     const newPeople = [...people.sort((a, b) => ((a.name > b.name) ? 1 : -1))];
     setPeople(newPeople);
   };
-
+  // First sorts alphabetically before second pass  sorting by amount.
   const sortByAmount = () => {
     people.sort((a, b) => ((a.name < b.name) ? 1 : -1));
     const newPeople = [...people.sort((a, b) => ((a.amount < b.amount) ? 1 : -1))];
@@ -43,14 +47,28 @@ const App = () => {
 
   return (
     <div>
+
+      <h1> Name Application </h1>
+
       Search:
-      <input value={search} onChange={searchHandler} />
+      <input value={search} onChange={searchHandler} type="text" />
+      <br />
+
       <button onClick={sortByName} type="submit">
         Sort by name
       </button>
       <button onClick={sortByAmount} type="submit">
         Sort by amount
       </button>
+
+      <h4>
+        Names in the list:
+        {` ${people.length}`}
+        <br />
+        Names that match search:
+        {` ${filteredPeople.length}`}
+      </h4>
+
       <table>
         <thead>
           <tr>
@@ -58,14 +76,14 @@ const App = () => {
             <th>AMOUNT</th>
           </tr>
         </thead>
+
         <tbody>
-          { filteredPeople.map((person) => <RenderPerson key={person.name} person={person} />)}
+          { filteredPeople.map(
+            (person) => <RenderPerson key={person.name} person={person} />,
+          )}
         </tbody>
       </table>
-      <h3>
-        Names in the list:
-        {` ${people.length}`}
-      </h3>
+
     </div>
   );
 };
